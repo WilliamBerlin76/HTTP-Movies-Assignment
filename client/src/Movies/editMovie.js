5920b476b2c1526acef9ceca79e8832eabbcb20b
WilliamBerlin76 <william.berlin76@gmail.com>
2019-10-17 15:12:17 -0500

    created basic static form

---
+++ client/src/Movies/editMovie.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import axion from 'axios';
+
+const EditForm = () => {
+
+    return(
+        <>
+        <h2>Edit Movie</h2>
+        <form>
+            <input/>
+            <input/>
+            <input/>
+            <input/>
+            <button>Save Changes</button>    
+        </form>
+        </>
+    )
+}
+
+export default EditForm;
